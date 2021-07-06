a0231c02d2da0a28d7864e3f49630a2aca01b44e
smartdev007 <kevadiajenish7@gmail.com>
2021-07-06 18:33:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CookIt!
+
+This is Heroku Deployment version. 
+
+Please checkout `master` branch for development version.
+
+### Live website
+
+https://cook-it-sw.herokuapp.com/
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,17 @@
+const usersResolvers = require('./users');
+const postsResolvers = require('./posts');
+const commentsResolvers = require('./comments');
+module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length
+  },
+  Query: {
+    ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation
+  }
+};
