1365aeafc34a8cec36ce31bfc34ab9102ad6690e
smartdev007 <kevadiajenish7@gmail.com>
2021-07-06 18:49:37 -0400

    change path

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  MONGODB: `${process.env.MONGO_URI}`,
+  MONGODB: `mongodb+srv://dbJenish:dbJenishCI@cluster0.hj5g1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   // "mongodb://localhost:27017/food",
   SECRET_KEY: 'some very secret key'
 };
